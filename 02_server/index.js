e72b5abef5eeb173d2488972a3d97c1e1a5ac8c7
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-25 07:20:08 +0530

    handle URLs in Node.js

--- 02_server/index.js
+++ 02_server/index.js
@@ -1,23 +1,31 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
     // console.log("New request received");
     // console.log(req.headers);
-    const log = `${Date.now()}: ${req.url} New request received\n`
+    const log = `${Date.now()}: ${req.url} New request received\n`;
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
-        switch(req.url) {
+        switch(myUrl.pathname) {
             case "/":
                 res.end("Home page");
                 break;
             case "/about":
-                res.end("About page");
+                const username = myUrl.query.myname
+                res.end(`Hi, ${username}`);
                 break;
             case "/contact-us":
                 res.end("contact-us")
                 break;
+            case "/search":
+                const search = myUrl.query.search_query
+                res.end("Here is you search for:"+ search)
+                break;
             default:
-                res.end("Nothing")
+                res.end("Not Found!")
         }
         
     })

463abf3b44437450845cdc1c3070ea2907c1f2b6
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-02-02 19:28:08 +0530

    Express js basics

--- 02_server/index.js
+++ 02_server/index.js
@@ -1,39 +1,38 @@
-const http = require("http")
-const fs = require("fs")
-const url = require("url")
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-    // console.log("New request received");
-    // console.log(req.headers);
-    const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
-    const myUrl = url.parse(req.url, true)
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch(myUrl.pathname) {
-            case "/":
-                if(req.method === "GET") res.end("Home page");
-                break;
-            case "/about":
-                const username = myUrl.query.myname
-                res.end(`Hi, ${username}`);
-                break;
-            case "/contact-us":
-                res.end("contact-us")
-                break;
-            case "/search":
-                const search = myUrl.query.search_query
-                res.end("Here is you search for:"+ search)
-                break;
-            case "/signup":
-                if(req.method === "GET") res.end("This is a sign up form")
-                else if(req.method === "POST"){
-                    // DB query
-                    res.end("Success!!!")
-                }
-            default:
-                res.end("Not Found!")
+  // console.log("New request received");
+  // console.log(req.headers);
+  const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
+  const myUrl = url.parse(req.url, true);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        if (req.method === "GET") res.end("Home page");
+        break;
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`Hi, ${username}`);
+        break;
+      case "/contact-us":
+        res.end("contact-us");
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here is you search for:" + search);
+        break;
+      case "/signup":
+        if (req.method === "GET") res.end("This is a sign up form");
+        else if (req.method === "POST") {
+          // DB query
+          res.end("Success!!!");
         }
-        
-    })
-})
+      default:
+        res.end("Not Found!");
+    }
+  });
+});
 
-myServer.listen(8000, () => console.log("Server started!!!"))
+myServer.listen(8000, () => console.log("Server started!!!"));

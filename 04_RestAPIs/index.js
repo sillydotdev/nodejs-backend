fb5525ff153f3a4ac0f82ac8f7084e6d37102ac1
Malik Kamran Mushtaq <kamran145422@gmail.com>
2024-03-26 08:00:32 +0530

    implement GET, POST, PATCH and DELETE routes

--- 04_RestAPIs/index.js
+++ 04_RestAPIs/index.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 const users = require("./MOCK_DATA.json");
+const { log } = require("console");
 
 const app = express();
+app.use(express.json());
+
 const PORT = 8000;
+
+// middlewares
+app.use(express.urlencoded({ extended: false }));
+
+function userExists(req, res, next) {
+  const userId = Number(req.params.id);
+  const user = users.find((user) => user.id === userId);
+  if (!user) {
+    return res.status(404).json({ message: "User not found..." });
+  }
+  req.user = user;
+  next();
+}
 
 // USING "https://mockaroo.com/" FOR MOCK DATA TO KEARN RestApiS
 
@@ -31,9 +49,13 @@ app.get("/api/users", (req, res) => {
 // });
 
 // POST request
-app.post("/api/users", (res, req) => {
+app.post("/api/users", (req, res) => {
   // create new user
-  return res.json({ status: "pending" });
+  const body = req.body;
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "Successfully added", id: users.length });
+  });
 });
 
 // PATCH request -- see below
@@ -43,7 +65,7 @@ app.post("/api/users", (res, req) => {
 // });
 
 // DELETE request -- see below
-// app.post("/api/users/:id", (res, req) => {
+// app.delete("/api/users/:id", (res, req) => {
 //   // delete a user
 //   return res.statusMessage("pending");
 // });
@@ -51,18 +73,40 @@ app.post("/api/users", (res, req) => {
 // If we notice, the routes for GET by id, PATCH and DELETE are the same, so we can merge them in a single group...
 app
   .route("/api/users/:id")
-  .get((req, res) => {
+  .get(userExists, async (req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+
     return res.json(user);
   })
-  .patch((res, req) => {
+  .patch(userExists, async (req, res) => {
     // edit user data
-    return res.json({ status: "pending" });
+    const userId = Number(req.params.id);
+    const updateUser = req.body;
+
+    const updatedUser = users.map((user) => {
+      if (user.id === userId) {
+        return { ...user, ...updateUser };
+      }
+      return user;
+    });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(updatedUser), (err) => {
+      if (err) {
+        res.statusCode(500).json({ message: "Internal server error" });
+      }
+      res.json({ message: "User Updated successfully" });
+    });
   })
-  .delete((res, req) => {
+  .delete(userExists, async (req, res) => {
     // delete a user
-    return res.json({ status: "pending" });
+    const userId = Number(req.params.id);
+    const updateData = users.filter((user) => user.id !== userId);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(updateData), (err) => {
+      if (err) {
+        res.statusCode(500).json({ message: "Internal server error" });
+      }
+      res.json({ message: "User deleted successfully" });
+    });
   });
 
 app.listen(PORT, () => {

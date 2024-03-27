6865a2f38ef71b685d62eb9c8903a9ed0cb8078f
Malik Kamran Mushtaq <kamran145422@gmail.com>
2024-03-27 08:36:37 +0530

    Connect MongoDB with express and implement CRUD operations

--- 04_RestAPIs/index.js
+++ 04_RestAPIs/index.js
@@ -54,9 +54,21 @@ app.get("/api/users", (req, res) => {
 app.post("/api/users", (req, res) => {
   // create new user
   const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields are required..." });
+  }
   users.push({ id: users.length + 1, ...body });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Successfully added", id: users.length });
+    return res
+      .status(201)
+      .json({ status: "Successfully added", id: users.length });
   });
 });
 

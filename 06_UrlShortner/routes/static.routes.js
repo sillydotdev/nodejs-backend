f483109941561320456dac6a200b5a0bbd7c4185
Malik Kamran Mushtaq <kamran145422@gmail.com>
2024-03-30 10:22:12 +0530

    rendering the short url on frontend

--- 06_UrlShortner/routes/static.routes.js
+++ 06_UrlShortner/routes/static.routes.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router();
+const URL = require("../models/url.model")
 
-router.get("/", (req, res) => {
-    res.render("home")
+router.get("/", async (req, res) => {
+    const allUrls = await URL.find({})
+    console.log(allUrls)
+    return res.render("home", {
+        urls: allUrls
+    })
 })
 
 module.exports = router;
